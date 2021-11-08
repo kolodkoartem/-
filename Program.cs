f743a277623c94e517242d023694bf445893fb1e
MartaLokit <93724240+MartaLokit@users.noreply.github.com>
2021-11-08 15:34:36 +0300

    Create Program.cs
    Задание 0

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Курсы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Pdirector : Video
+    {
+        public string Name;
+    }
+    class Painter : Picter
+    {
+        public string Name;
+    }
+    class Singer:Song
+    {
+        public string Name;
+    }
+    abstract class Media:Action
+    {
+        public abstract void extension();
+        public abstract void name();
+        public abstract void path();
+        public abstract void quality();   
+    }
+    class Video:Media
+    {
+        public override void extension() { }
+        public override void name() { }
+        public override void path() { }
+        public override void quality() { }
+    }
+    class Song : Media
+    {
+        public override void extension() { }
+        public override void name() { }
+        public override void path() { }
+        public override void quality() { }
+    }
+    class Picter : Media
+    {
+        public override void extension() { }
+        public override void name() { }
+        public override void path() { }
+        public override void quality() { }
+    }
+    class Action : Delete, Add, Search
+    {
+        public void delete(){}
+        public void add() { }
+        public void search() { }
+    }
+    interface Delete
+    {
+        void delete();
+    }
+    interface Add
+    {
+        void add();
+    }
+    interface Search
+    {
+        void search();
+    }
+
+}
